da5bc25fd449da1bb3119e9406313496e8dc637b
MichaelFadyeyev <mihail.fadeev@ukr.net>
2025-01-22 00:53:58 +0200

    Add projects

---
+++ VideoStrimmingDemoAPI/Controllers/VideoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace VideoStrimmingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+        
+        [HttpGet("{fileName}")]
+        public IActionResult GetVideo(string fileName)
+        {
+            var currentDir = Directory.GetCurrentDirectory(); 
+            var filePath = Path.Combine("Videos", fileName);
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+            return PhysicalFile(filePath, "video/mp4", enableRangeProcessing: true);
+        }
+    }
+}
